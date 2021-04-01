cf3db158c89c89ea32d35124dbe5ff2683101b9e
Vu Quach <vuquach@VuMacPro.local>
2021-04-01 18:07:13 +1100

    trying to fix error

--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-const publicPath = path.join(__dirname, 'public');
-
+const publicPath = path.join(__dirname, '..', 'public');
+console.log(publicPath);
 app.use(express.static(publicPath));
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
